d2b2c99501af8e378ed613b1b48c87063250fe8f
Jay Witt <twizzalt@jaylan.net>
2022-11-18 08:04:00 -0600

    Added VM Maintenance

--- Alerts/README.md
+++ Alerts/README.md
@@ -7,3 +7,11 @@ You can use the [DumpAllAlertsByEmail](https://github.com/JayWitt/AzureOperation
 You can use the [DumpAllAlerts](https://github.com/JayWitt/AzureOperationGuide/blob/master/Alerts/DumpAllAlerts.ps1) to get the list of all of the alert rule types. 
 
 NOTE: That you will need to install the [ExportExcel](https://www.powershellgallery.com/packages/ImportExcel/1.90/Content/Export-Excel.ps1) Powershell module.
+
+
+# VM Maintenance
+
+You can use the [VMMaintenance-Report](https://github.com/JayWitt/AzureOperationGuide/blob/master/Alerts/VMMaintenance-Report.ps1) script to get a list of VMs that are going to be impacted by planned maintenance. The script has two sets of variables that would need to be updated. You would need to enter in the subscription ID(s) of the subscriptions that you would want to search through and also provide the region. This script is meant to be helpful to respond to specific Service Health Alerts that report planned maintenance on a subset of VMs.
+
+The $output variable will store the list of machines that have maintenance coming up in the future.
+The $outputfull variable will store the full list of VMs in the subscription and region specified.
---
+++ Alerts/VMMaintenance-Report.ps1
@@ -0,0 +1,75 @@
+﻿
+$subs = @('<subscription ID #1>','<subscription ID #2>')
+$loc = "<Azure Region shorthand (i.e. centrlaus)>"
+
+$output = @()
+$outputfull = @()
+
+Foreach ($subId in $subs)
+{
+    set-azcontext -Subscription $subId -WarningAction SilentlyContinue
+
+    $query = "resources | where type == 'microsoft.compute/virtualmachines' | where subscriptionId == '$subID' | where location == '$loc' | join kind=leftouter (ResourceContainers | where type=='microsoft.resources/subscriptions' | project SubscriptionName=name,subscriptionId) on subscriptionId | project SubscriptionName, resourceGroup, name"
+
+    $results = Search-AzGraph -query $query -First 1000
+
+    if ($results.count -eq 1000)
+    {
+        $rowcount = $results.Count
+        $done = 0
+        do {
+            $tmpResults = Search-AzGraph -Query $query -Skip $rowcount -First 1000
+            $rowcount += $tmpResults.Count
+            $results += $tmpResults
+            if ($tmpResults.count -lt 1000) {$done = 1}
+        }
+        while ($done -eq 0)
+    }
+
+    write-host -ForegroundColor Magenta "Query: $query"
+    write-host -ForegroundColor Green "Count: $($results.count)"
+
+    foreach ($vm in $results)
+    {
+        $mainAllow = ""
+        $preMainStart = ""
+        $preMainEnd = ""
+        $mainStart = ""
+        $mainEnd = ""
+
+        $VMName = $vm.Name
+        $VMRG = $vm.resourceGroup
+        $SubName = $vm.SubscriptionName
+
+        $tmp = Get-AzVM -ResourceGroupName $VMRG -Name $VMName -Status -ErrorAction SilentlyContinue
+        $mainAllow = $tmp.MaintenanceRedeployStatus.IsCustomerInitiatedMaintenanceAllowed
+        $preMainStart = $tmp.MaintenanceRedeployStatus.PreMaintenanceWindowStartTime
+        $preMainEnd = $tmp.MaintenanceRedeployStatus.PreMaintenanceWindowEndTime
+        $mainStart = $tmp.MaintenanceRedeployStatus.MaintenanceWindowStartTime
+        $mainEnd = $tmp.MaintenanceRedeployStatus.MaintenanceWindowsEndTime
+        $LastOpCode = $tmp.MaintenanceRedeployStatus.LastOperationResultCode
+        $LastOpMsg = $tmp.MaintenanceRedeployStatus.LastOperationMessage
+
+        write-host "$VMName - $mainAllow - $preMainStart"
+        $obj = New-Object -TypeName psobject
+        $obj | Add-Member -MemberType NoteProperty -Name SubName -Value $subName
+        $obj | Add-Member -MemberType NoteProperty -Name SubID -Value $subID
+        $obj | Add-Member -MemberType NoteProperty -Name ResourceGroup -Value $VMRG
+        $obj | Add-Member -MemberType NoteProperty -Name VMName -Value $VMName
+        $obj | Add-Member -MemberType NoteProperty -Name PremtiveStart -Value $preMainStart
+        $obj | Add-Member -MemberType NoteProperty -Name PremtiveEnd -Value $preMainEnd
+        $obj | Add-Member -MemberType NoteProperty -Name MainStart -Value $MainStart
+        $obj | Add-Member -MemberType NoteProperty -Name MainEnd -Value $MainEnd
+        $obj | Add-Member -MemberType NoteProperty -Name LastOperationResultCode -Value $LastOpCode
+        $obj | Add-Member -MemberType NoteProperty -Name LastOperationMessage -Value $LastOpMsg
+
+        $outputfull += $obj
+
+        if (($preMainEnd -gt (get-date)) -or ($MainEnd -gt (get-date)))
+        {
+            $output += $obj
+        }
+    }
+
+}
+
